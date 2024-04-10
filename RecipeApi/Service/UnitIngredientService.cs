6ee937d3739d924e111a19fc4ce7b2ba6bd30e76
Krzysztof Kokot <krzysztofkokot@Krzysztofs-MacBook-Air.local>
2024-04-10 09:49:24 +0200

    create service

---
+++ RecipeApi/Service/UnitIngredientService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RecipeApi.IService;
+
+namespace RecipeApi.Service
+{
+    public class UnitIngredientService : IUnitIngredientService
+    {
+        
+    }
+}
